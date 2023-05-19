7c3e57b12ad14e0bf4124ff30f3c41ede92c53f5
Emmy Bishop <miaui96@gmail.com>
2023-05-19 16:33:18 -0500

    Added model definitions

---
+++ server/db/models.js
@@ -0,0 +1,84 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./index');
+
+const numericRangeValidator = (min, max) => ({
+  isNumeric: {
+    msg: 'Field must be a number'
+  },
+  min: {
+    args: [min],
+    msg: `Field must be greater than or equal to ${min}`
+  },
+  max: {
+    args: [max],
+    msg: `Field must be less than or equal to ${max}`
+  }
+});
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  age: {
+    type: DataTypes.INTEGER
+  },
+  maxTravelDist: {
+    type: DataTypes.INTEGER
+  },
+  sober: {
+    type: DataTypes.BOOLEAN
+  },
+  canHost: {
+    type: DataTypes.BOOLEAN
+  },
+  DM: {
+    type: DataTypes.STRING
+  },
+  combatHeaviness: {
+    type: DataTypes.INTEGER,
+    validate: numericRangeValidator(1, 5)
+  },
+  strategyHeaviness: {
+    type: DataTypes.INTEGER,
+    validate: numericRangeValidator(1, 5)
+  },
+  roleplayFocus: {
+    type: DataTypes.INTEGER,
+    validate: numericRangeValidator(1, 5)
+  },
+  storyFocus: {
+    type: DataTypes.INTEGER,
+    validate: numericRangeValidator(1, 5)
+  },
+  friends: {
+    type: DataTypes.JSON,
+    defaultValue: []
+  }
+});
+
+const Message = sequelize.define('message', {
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  message: {
+    type: DataTypes.STRING
+  }
+});
+
+User.hasMany(Message, { foreignKey: 'userId' });
+Message.belongsTo(User, { foreignKey: 'userId' });
+
+module.exports = {
+  User,
+  Message
+};
