70e5965ae8ffe8cffc711f530a9778689169aa0a
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-19 14:57:06 -0500

    Add basic About page

---
+++ client/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <main>
+      Fellowship Finder is a web app that allows users to find and
+      meet people with similar interests in games.
+    </main>
+  );
+}
+
+export default About;
