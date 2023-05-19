c7a0c5568f8b98bf34c0943ac4406e2b06f03549
Emmy Bishop <miaui96@gmail.com>
2023-05-19 13:01:29 -0500

    Add port number to server index

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 
-// const PORT = 7000;
+const PORT = 7000;
 const PUBLIC_IP = '18.217.208.71';
 
-app.listen(PUBLIC_IP, (err) => {
+app.listen(PORT, PUBLIC_IP, (err) => {
   if (err) {
     console.log('server connection failed', err);
   }
-  console.log(`Page running at: ${PUBLIC_IP}`);
+  console.log(`Page running at: ${PUBLIC_IP}:${PORT}`);
 });
