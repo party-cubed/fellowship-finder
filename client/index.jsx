aa593cc7141291c8b0ee180f7de6c556bb705891
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-19 14:56:28 -0500

    Moved App into index.jsx and deleted App.jsx and added router

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
+import { StrictMode } from 'react';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import About from './components/About';
+import NoPage from './components/NoPage';
 
-const container = document.getElementById('app');
+export default function App() {
+  return (
+    <StrictMode>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </StrictMode>
+  );
+}
 
-const root = createRoot(container);
-
+const root = createRoot(document.getElementById('app'));
 root.render(<App />);
