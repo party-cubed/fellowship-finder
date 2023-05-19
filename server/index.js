2c9341ed30f0e9c37d00cd2c050d5813859938f8
Emmy Bishop <miaui96@gmail.com>
2023-05-19 13:14:17 -0500

    Remove public IP from server index

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const app = require('./app');
 
 const PORT = 7000;
-const PUBLIC_IP = '18.217.208.71';
 
-app.listen(PORT, PUBLIC_IP, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log('server connection failed', err);
   }
-  console.log(`Page running at: ${PUBLIC_IP}:${PORT}`);
+  console.log(`Page running at port ${PORT}`);
 });
