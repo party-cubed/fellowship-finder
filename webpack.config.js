675ebe56985f757ee50dee0e7f60deab71379554
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-19 14:01:38 -0500

    Add resolve extensions

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     rules: [
       {
