65d1ebb38d003b8b300de3f1a273a3458f40e356
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-22 08:59:43 -0500

    Add generic user get request

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
-const userModel = require('../models/Users.js');
-console.log(userModel.findByPk(1));
+const { Users } = require('../models/init-models.js');
+
 const User = Router();
+
+User.get('/', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const users = await Users.findAll();
+    return res.json(users);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'An error occurred while retrieving users' });
+  }
+});
 
 User.get('/:id', async (req, res) => {
   const { id } = req.params;
+  console.log('id', id);
   try {
-    const user = await userModel.findByPk(id);
+    console.log(Users.findByPk);
+    const user = await Users.findByPk(id);
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
