c9e8ac2e97184f560ed88804770d521b5c44cea2
Emmy Bishop <miaui96@gmail.com>
2023-05-22 15:58:51 -0500

    Update route order

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { User } = require('./db/models');
-// const User = require('./routers/userRouter');
+const User = require('./routers/userRouter');
 const { sequelize } = require('./db/index');
 
 // initilize App
@@ -14,28 +13,30 @@ app.use(express.static(clientPath));
 // configure App
 app.use(express.json());
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../dist/index.html'), (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send(err);
-//     }
-//   });
-// });
-
-// ADD APP ROUTERS
-app.get('/api/users', (req, res) => {
-  User.findAll()
-    .then((users) => {
-      res.status(200).send(users);
-    })
-    .catch((err) => {
-      console.error('Failed to FIND ALL users:', err);
-      res.sendStatus(500);
-    });
-});
-
 //ROUTERS
 app.use('/api/user', User);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'), (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+  });
+});
+
+// ADD APP ROUTERS
+// app.get('/api/users', (req, res) => {
+//   User.findAll()
+//     .then((users) => {
+//       res.status(200).send(users);
+//     })
+//     .catch((err) => {
+//       console.error('Failed to FIND ALL users:', err);
+//       res.sendStatus(500);
+//     });
+// });
+
+
 
 module.exports = app;
