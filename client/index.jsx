a3500709f37596a3ac531a205e3b8b6cc2eb8e1e
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-23 13:40:34 -0500

    Add darkmode theme

--- client/index.jsx
+++ client/index.jsx
@@ -2,42 +2,54 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { StrictMode, useState } from 'react';
-import Home from './components/Home';
-import Layout from './components/Layout';
-import About from './components/About';
-import NoPage from './components/NoPage';
-import Welcome from './components/Welcome';
-import Signup from './components/Signup';
-import Signin from './components/Signin';
-import ProfilePage from './components/ProfilePage';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import About from './pages/About';
+import NoPage from './pages/NoPage';
+import Welcome from './pages/Welcome';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import ProfilePage from './pages/ProfilePage';
 import GoogleOAuth from './components/GoogleOAuth';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function App() {
   const [user, setUser] = useState({});
   return (
     <StrictMode>
-      <Router>
-        <GoogleOAuth setUser={setUser} />
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/welcometraveler" element={<Welcome />} />
-          <Route
-            path="*"
-            element={(
-              <Layout>
-                <Routes>
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/signin" element={<Signin />} />
-                  <Route path="/user/:id" element={<ProfilePage />} />
-                  <Route path="*" element={<NoPage />} />
-                </Routes>
-              </Layout>
-            )}
-          />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline>
+          <Router>
+            <GoogleOAuth setUser={setUser} />
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route path="/welcometraveler" element={<Welcome />} />
+              <Route
+                path="*"
+                element={(
+                  <Layout>
+                    <Routes>
+                      <Route path="/home" element={<Home />} />
+                      <Route path="/about" element={<About />} />
+                      <Route path="/signup" element={<Signup />} />
+                      <Route path="/signin" element={<Signin />} />
+                      <Route path="/user/:id" element={<ProfilePage />} />
+                      <Route path="*" element={<NoPage />} />
+                    </Routes>
+                  </Layout>
+                )}
+              />
+            </Routes>
+          </Router>
+        </CssBaseline>
+      </ThemeProvider>
     </StrictMode>
   );
 }
