1f1cbc148e2f9ae58f745dc8fc1026938e2dd92f
Marvas <marvasmc@gmail.com>
2023-05-24 18:20:47 -0500

    uncommented index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
 
-
-
 const { default: axios } = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const passport = require('passport');
-// const expressSession = require('express-session');
-// const cookieParser = require('cookie-parser');
-// const bycrypt = require('bcrypt');
-// const db = require('./db/index');
+const passport = require('passport');
+const expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
+const bycrypt = require('bcrypt');
+const db = require('./db/index');
 
 
 const app = require('./app');
 
 const PORT = 3001;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(expressSession({ secret: 'mySecretKey', resave: false, saveUninitialized: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession({ secret: 'mySecretKey', resave: false, saveUninitialized: false }));
 
 app.use(cors({ origin: true }));
 app.use(cors({
@@ -26,97 +24,97 @@ app.use(cors({
   credentials: true
 }));
 
-// app.use(cookieParser('mySecretKey'));
+app.use(cookieParser('mySecretKey'));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-// //require('./passportConfig')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passportConfig')(passport);
 
-// const { User } = require('./db/models'); // Assuming you have a User model defined
+const { User } = require('./db/models'); // Assuming you have a User model defined
 
-// app.post('/signup', async (req, res) => {
-//   const {
-//     username,
-//     password,
-//     email,
-//     age,
-//     maxTravelDist,
-//     sober,
-//     canHost,
-//     DM,
-//     combatHeaviness,
-//     strategyHeaviness,
-//     roleplayFocus,
-//     storyFocus
-//   } = req.body;
+app.post('/signup', async (req, res) => {
+  const {
+    username,
+    password,
+    email,
+    age,
+    maxTravelDist,
+    sober,
+    canHost,
+    DM,
+    combatHeaviness,
+    strategyHeaviness,
+    roleplayFocus,
+    storyFocus
+  } = req.body;
 
-//   try {
-//     const existingUser = await User.findOne({ where: { username } });
-//     if (existingUser) {
-//       res.send({ message: 'Username already exists' });
-//     } else {
-//       const hashedPassword = bycrypt.hashSync(password, 10);
-//       await User.create({
-//         username,
-//         password: hashedPassword,
-//         email,
-//         age,
-//         maxTravelDist,
-//         sober,
-//         canHost,
-//         DM,
-//         combatHeaviness,
-//         strategyHeaviness,
-//         roleplayFocus,
-//         storyFocus
-//       });
-//       res.send({ message: 'User created' });
-//     }
-//   } catch (error) {
-//     console.error('Error during signup:', error);
-//     res.status(500).send({ message: 'An error occurred during signup' });
-//   }
-// });
+  try {
+    const existingUser = await User.findOne({ where: { username } });
+    if (existingUser) {
+      res.send({ message: 'Username already exists' });
+    } else {
+      const hashedPassword = bycrypt.hashSync(password, 10);
+      await User.create({
+        username,
+        password: hashedPassword,
+        email,
+        age,
+        maxTravelDist,
+        sober,
+        canHost,
+        DM,
+        combatHeaviness,
+        strategyHeaviness,
+        roleplayFocus,
+        storyFocus
+      });
+      res.send({ message: 'User created' });
+    }
+  } catch (error) {
+    console.error('Error during signup:', error);
+    res.status(500).send({ message: 'An error occurred during signup' });
+  }
+});
 
-// app.post('/signin', (req, res, next) => {
-//   passport.authenticate('local', (err, user, info) => {
-//     if (err) {
-//       throw err;
-//     }
-//     if (!user) {
-//       res.send('No user exists');
-//     }
-//     if (user) {
-//       // eslint-disable-next-line no-shadow
-//       req.login(user, (err) => {
-//         if (err) {
-//           throw err;
-//         }
-//         res.send({
-//           message: 'User signed in',
-//           user
-//         });
-//       });
-//     }
-//   })(req, res, next);
-// });
+app.post('/signin', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      throw err;
+    }
+    if (!user) {
+      res.send('No user exists');
+    }
+    if (user) {
+      // eslint-disable-next-line no-shadow
+      req.login(user, (err) => {
+        if (err) {
+          throw err;
+        }
+        res.send({
+          message: 'User signed in',
+          user
+        });
+      });
+    }
+  })(req, res, next);
+});
 
-// // eslint-disable-next-line consistent-return
-// function ensureAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     return next();
-//   }
-//   res.redirect('/signin');
-// }
+// eslint-disable-next-line consistent-return
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/signin');
+}
 
-// app.get('/getUser', ensureAuthenticated, (req, res) => {
-//   res.send(req.user);
-// });
+app.get('/getUser', ensureAuthenticated, (req, res) => {
+  res.send(req.user);
+});
 
 
-// app.get('/getUser', (req, res) => {
-//   res.send(req.user);
-// });
+app.get('/getUser', (req, res) => {
+  res.send(req.user);
+});
 
 
 app.use(express.json());
@@ -136,8 +134,6 @@ app.post('/authenticate', async (req, res) => {
     return res.status(err.response.status).json(err.response.data);
   }
 });
-
-
 
 
 app.listen(PORT, (err) => {
