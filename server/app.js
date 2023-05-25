6542aae7894258a204c5f9d3b85bd146fe1d4ee3
Emmy Bishop <miaui96@gmail.com>
2023-05-25 15:08:46 -0500

    Install cookie-session

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session'); //
 const User = require('./routers/userRouter');
 const authRoutes = require('./routers/authRouter'); //
 const passportSetup = require('../config/passport-setup'); //
