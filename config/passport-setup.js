e344084b7884792c339ef85c199c53ccf49f81ed
Emmy Bishop <miaui96@gmail.com>
2023-05-25 14:34:58 -0500

    Add functionality to check if user already exists and if not, create user w/ googleId

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const { User: Users } = require('../server/db/models');
 
 passport.use(
   new GoogleStrategy({
@@ -8,9 +9,35 @@ passport.use(
     callbackURL: '/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-  }, () => {
-  // passport cb function
-    console.log('passport callback function fired');
+    // passport cb function
+  }, (accessToken, refreshToken, profile, done) => {
+    // accessToken gives access to user info
+    // refreshToken refreshes accessToken
+    // profile is user info
+    // done is a func to call when done w/ cb
+    const { id, emails } = profile;
+
+    // check if user already exists
+    Users.findOne({ where: { googleId: id } })
+      .then((currentUser) => {
+        if (currentUser) {
+          console.log('user is:', currentUser);
+        } else {
+          Users.create({
+            googleId: id,
+            email: emails[0].value
+          })
+            .then((newUser) => {
+              console.log('new user created:', newUser);
+            })
+            .catch((err) => {
+              console.error('Failed to CREATE new user:', err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.error('Failed to FIND user by googleId:', err);
+      });
   })
 );
 
