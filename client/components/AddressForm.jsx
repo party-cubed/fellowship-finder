dc1d97b26b4523ff401429e26f3b7a2399d91254
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-25 14:15:56 -0500

    Removed heading and changed widths

--- client/components/AddressForm.jsx
+++ client/components/AddressForm.jsx
@@ -15,46 +15,50 @@ const AddressForm = ({ address, setAddress }) => {
 
   return (
     <Container>
-      <Typography variant="h5">Address</Typography>
       <Tabs value={isinperson} onChange={handleChange}>
         <Tab label="In Person" />
         <Tab label="Online" />
       </Tabs>
       {isinperson === 0 && (
-        <Box component="form" noValidate autoComplete="off">
+        <Box component="form" noValidate autoComplete="off" sx={{ width: '90%' }}>
           <TextField
             name="street"
             label="Street"
             value={address.street}
             onChange={handleInputChange}
+            sx={{ width: '100%' }}
           />
           <TextField
             name="city"
             label="City"
             value={address.city}
             onChange={handleInputChange}
+            sx={{ width: '100%' }}
           />
           <TextField
             name="state"
             label="State"
             value={address.state}
             onChange={handleInputChange}
+            sx={{ width: '100%' }}
           />
           <TextField
             name="zip"
             label="Zip Code"
             value={address.zip}
             onChange={handleInputChange}
+            sx={{ width: '100%' }}
           />
         </Box>
       )}
       {isinperson === 1 && (
-        <Box>
+        <Box component="form" noValidate autoComplete="off" sx={{ width: '90%' }}>
           <TextField
             name="link"
             label="Link"
             value={address.link}
             onChange={handleInputChange}
+            sx={{ width: '100%' }}
           />
         </Box>
       )}
