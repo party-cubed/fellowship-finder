6d695ab18f08e82bcd153e19f9950c3532e57873
Emmy Bishop <miaui96@gmail.com>
2023-05-25 16:18:19 -0500

    Add function to check if user logged in and if not, redirect to login

--- server/routers/profileRouter.js
+++ server/routers/profileRouter.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send(`you are logged in, this is your profile - ${req.user.username}`);
+const authCheck = (req, res, next) => {
+  // check if user NOT logged in
+  if (!req.user) {
+    res.redirect('/auth/login');
+  } else {
+    // move to next function
+    next();
+  }
+};
+
+// fire authCheck to check if user logged in before displaying profile
+router.get('/', authCheck, (req, res) => {
+  res.send(`You are logged in, this is your profile: ${req.user.email}`);
 });
 
 module.exports = router;
