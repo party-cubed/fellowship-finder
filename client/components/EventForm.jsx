981d0d9ef1e72e1b9d5c5027a64d164410a47f1f
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-26 09:54:34 -0500

    Create loading block if no event and put in day.js conversions

--- client/components/EventForm.jsx
+++ client/components/EventForm.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import {
   Box, Grid, Paper, Button, Typography, Container, TextField
 } from '@mui/material';
@@ -8,40 +9,41 @@ import UsersSelect from './UsersSelect';
 
 
 const EventForm = ({ event, setEventValue, users }) => {
+  useEffect(() => {
+    console.log(event, 'event in EventForm');
+  });
+
+  if (!event) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      {event
-        ? (
-          <>
-            <Container sx={{ marginBottom: '20px' }}>
-              <Box component="form" noValidate autoComplete="off" sx={{ width: '90%' }}>
-                <TextField
-                  name="link"
-                  label="Event Name"
-                  value={event ? event.title : ''}
-                  onChange={({ target: { value } }) => setEventValue('title', value)}
-                  sx={{ width: '100%' }}
-                />
-              </Box>
-            </Container>
-            <Container>
-              <DateSelect date={event ? event.start : dayjs(new Date())} label="Start" setSelectedDate={(date) => setEventValue('start', date)} />
-              <DateSelect date={event ? event.end : dayjs(new Date())} label="End" setSelectedDate={(date) => setEventValue('end', date)} />
-            </Container>
-            <AddressForm address={event ? event.address : ''} setAddress={(address) => setEventValue('address', address)} sx={{ width: '90%' }} />
-            <Container sx={{ marginTop: '20px', marginBottom: '20px' }}>
-              <UsersSelect
-                users={users || []}
-                label="Invites"
-                selectedUsers={event.selectedUsers || []}
-                setSelectedUsers={(selectedUsers) => setEventValue('selectedUsers', selectedUsers)}
-              />
-            </Container>
-          </>
-        ) : (
-          <Container />
-        )}
-    </div>
+    <>
+      <Container sx={{ marginBottom: '20px' }}>
+        <Box component="form" noValidate autoComplete="off" sx={{ width: '90%' }}>
+          <TextField
+            name="link"
+            label="Event Name"
+            value={event ? event.title : ''}
+            onChange={({ target: { value } }) => setEventValue('title', value)}
+            sx={{ width: '100%' }}
+          />
+        </Box>
+      </Container>
+      <Container>
+        <DateSelect date={event ? dayjs(event.start) : dayjs(new Date())} label="Start" setSelectedDate={(date) => setEventValue('start', date)} />
+        <DateSelect date={event ? dayjs(event.end) : dayjs(new Date())} label="End" setSelectedDate={(date) => setEventValue('end', date)} />
+      </Container>
+      <AddressForm address={event ? event.address : ''} setAddress={(address) => setEventValue('address', address)} sx={{ width: '90%' }} />
+      <Container sx={{ marginTop: '20px', marginBottom: '20px' }}>
+        <UsersSelect
+          users={users || []}
+          label="Invites"
+          selectedUsers={event.selectedUsers || []}
+          setSelectedUsers={(selectedUsers) => setEventValue('selectedUsers', selectedUsers)}
+        />
+      </Container>
+    </>
   );
 };
 
