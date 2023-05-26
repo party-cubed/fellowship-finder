73a31820c87f557f79596d7d7caa378d0e5cedb1
Emmy Bishop <miaui96@gmail.com>
2023-05-26 10:13:17 -0500

    Remove non-functional prop assignment to Search

--- client/index.jsx
+++ client/index.jsx
@@ -50,7 +50,7 @@ export default function App() {
                         <Route path="/about" element={<About />} />
                         <Route path="/events" element={<Events />} />
                         <Route path="/user/:id" element={<Profile />} />
-                        <Route path="/search" element={<Search currUser={user} />} />
+                        <Route path="/search" element={<Search />} />
                         <Route path="/auth/login" element={<Login />} />
                         <Route path="*" element={<NoPage />} />
                       </Routes>
