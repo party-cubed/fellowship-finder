209407ce502aa53f6a571eabf92ebce5cac85297
Emmy Bishop <miaui96@gmail.com>
2023-05-27 17:24:06 -0500

    Fix UserProvider

--- client/components/UserProvider.jsx
+++ client/components/UserProvider.jsx
@@ -39,9 +39,9 @@ export function UserProvider({ children }) {
       });
   };
 
-  // useEffect(() => {
-  //   getUser();
-  // }, []);
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <UserContext.Provider value={{ activeUser, setActiveUser }}>
