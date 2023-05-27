8887f77425a4b140c5a1fc9cebb75a49f49c7131
Marvas <marvasmc@gmail.com>
2023-05-27 15:14:47 -0500

    got chat auto loading

--- client/pages/ChatPageStuff/AuthPage.jsx
+++ client/pages/ChatPageStuff/AuthPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 
 import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
@@ -10,32 +11,67 @@ const AuthPage = (props) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    console.log(activeUser);
+    if (activeUser) {
+      const { username } = activeUser;
+
+      axios
+        .post('http://localhost:3001/authenticate', { username })
+        .then((r) => {
+          const data = { ...r.data, secret: username };
+          // eslint-disable-next-line react/prop-types
+          props.onAuth(data);
+        })
+        .catch((err) => console.error('Error: ', err));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeUser]);
 
   return (
-    <div className="background">
-      <form>
-        <div className="form-title">Welcome 👋</div>
-
-        <div className="form-subtitle">Set a username to get started</div>
-
-        <div className="auth">
-          <div className="auth-label">Username</div>
-          <input className="auth-input" name="username" />
-          <button className="auth-button" type="submit">
-            Enter
-          </button>
-        </div>
-      </form>
-    </div>
+    <div> ''</div>
   );
 };
 
 export default AuthPage;
 
 
-// we dont wanna see this screen. the assumption is that we're already signed in. we dont
-// wanna see this screen. maybe on line 31 we make an axios request to the database
-// and get the signed in users username, dont even show this pageXOffset, auto fill the input
-// with the username and that gets us into the chat
+// import axios from 'axios';
+
+
+// const AuthPage = (props) => {
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+//     const { value } = e.target[0];
+
+//     axios.post(
+//       'http://localhost:3001/authenticate',
+//       { username: value }
+//     )
+//       // eslint-disable-next-line react/destructuring-assignment, react/prop-types
+//       .then((r) => props.onAuth({ ...r.data, secret: value }))
+//       .catch((err) => console.error('Error: ', err));
+
+//     // eslint-disable-next-line react/destructuring-assignment, react/prop-types
+//     props.onAuth({ username: value, secret: value });
+//   };
+
+//   return (
+//     <div className="background">
+//       <form onSubmit={onSubmit} className="form-card">
+//         <div className="form-title">Welcome 👋</div>
+
+//         <div className="form-subtitle">Set a username to get started</div>
+
+//         <div className="auth">
+//           <div className="auth-label">Username</div>
+//           <input className="auth-input" name="username" />
+//           <button className="auth-button" type="submit">
+//             Enter
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AuthPage;
+
