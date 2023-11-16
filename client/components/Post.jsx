91d3c8f5ec85a72403f655f75dd43f046cdc9ec3
jshep23prog <jshep23@gmail.com>
2023-11-15 20:23:51 -0600

    "add/ adding use state to Post list and trying to pass down props to post so I can start rendering"

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -2,9 +2,11 @@ import React from "react";
 //CHILD OF POSTLIST
 
 function Post(props) {
+  console.log('post props', props)
   return (
     <div>
       <h5>Post component test</h5>
+      <div>{props.message.post}</div>
     </div>
   );
 }
--- client/pages/PostList.jsx
+++ client/pages/PostList.jsx
@@ -1,18 +1,40 @@
 /* eslint-disable max-len */
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import Post from '../components/Post';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Post from '../components/Post';
 
 //CHILD OF APP, PARENT OF POST
 
-// function PostList() {
-//   return (
-//     <div className="postlist-container">
-//       <h3>POSTLIST TESTER</h3>
-//       <Post />
-//     </div>
-//   );
-// }
+function PostList() {
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/post/all')
+      .then((postArray) => {
+        console.log('postArray', postArray);
+        setAllPosts(postArray);
+      })
+      .catch((err) => {
+        console.error('Failed to get posts to render', err);
+      });
+  }, []);
+
+  console.log('allPosts', allPosts.data);
+  const postDataArray = allPosts.data;
+  return (
+    <div className="postlist-container">
+      <h3>POSTLIST TESTER</h3>
+      <div>
+        {/* {postDataArray.map((post) => ( */}
+        <Post
+          message={postDataArray}
+        />
+
+        {/* // ))} */}
+      </div>
+    </div>
+  );
+}
 
 // export default PostList;
 
