37147044e5c1221b488cfe8d6fae9468d4b23b12
gfish94 <grmyfisher@gmail.com>
2023-11-16 16:30:57 -0600

    rendering stat graph

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -1,26 +1,15 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import {
-  Typography,
-  Grid,
-  Container,
-  Chip,
-  Avatar,
-  Table,
-  TableHead,
-  TableContainer,
-  TableRow,
-  TableCell,
-  Paper,
-} from '@mui/material';
-import profilepic from '../assets/profilepic.jpg';
-import { UserContext } from '../components/UserProvider';
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Typography, Grid, Container, Chip, Avatar } from "@mui/material";
+import CharSheet from "./CharSheet.jsx";
+import profilepic from "../assets/profilepic.jpg";
+import { UserContext } from "../components/UserProvider";
 
 const Profile = () => {
   const { activeUser, setActiveUser } = useContext(UserContext);
   const { id } = useParams();
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   const [sheets, setSheets] = useState([]);
 
   useEffect(() => {
@@ -29,7 +18,7 @@ const Profile = () => {
         const { data } = await axios.get(`api/user/${id}`);
         setUser(data);
       } catch (err) {
-        console.error('Error fetching user data: ', err);
+        console.error("Error fetching user data: ", err);
       }
     };
     fetchUser();
@@ -50,10 +39,10 @@ const Profile = () => {
         sx={{
           width: 150,
           height: 150,
-          position: 'relative',
-          top: '-50px',
-          left: '-20%',
-          margin: '0 auto',
+          position: "relative",
+          top: "-50px",
+          left: "-20%",
+          margin: "0 auto",
         }}
         alt="Marvy Warvy"
         src={user.image ? user.image : profilepic}
@@ -67,59 +56,44 @@ const Profile = () => {
           lg={4}
           xl={3}
         >
-          <Typography variant="h2">{user ? user.username : ''}</Typography>
+          <Typography variant="h2">{user ? user.username : ""}</Typography>
           <Chip
-            label={`Email: ${user ? user.email : ''}`}
+            label={`Email: ${user ? user.email : ""}`}
             variant="outlined"
           />
           <Chip
-            label={`Companions: ${user.friends ? user.friends : 'Nary a one'}`}
+            label={`Companions: ${user.friends ? user.friends : "Nary a one"}`}
             variant="outlined"
           />
           <Chip
-            label={`Combat Heaviness: ${user ? user.combatHeaviness : ''}`}
+            label={`Combat Heaviness: ${user ? user.combatHeaviness : ""}`}
             variant="outlined"
           />
           <Chip
-            label={`Strategy Heaviness: ${user ? user.strategyHeaviness : ''}`}
+            label={`Strategy Heaviness: ${user ? user.strategyHeaviness : ""}`}
             variant="outlined"
           />
           <Chip
-            label={`Roleplay Focus: ${user ? user.roleplayFocus : ''}`}
+            label={`Roleplay Focus: ${user ? user.roleplayFocus : ""}`}
             variant="outlined"
           />
           <Chip
-            label={`Story Focus: ${user ? user.storyFocus : ''}`}
+            label={`Story Focus: ${user ? user.storyFocus : ""}`}
             variant="outlined"
           />
         </Grid>
       </Grid>
 
-      {sheets.map((sheet) => (
-        <TableContainer key={sheet.id} component={Paper} style={{margin: '100px', border: '10px white solid round' }}>
-          {/* refactor to dividers */}
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name: {sheet.charName}</TableCell>
-                <TableCell>Race: {sheet.charRace}</TableCell>
-                <TableCell>Class: {sheet.charClass}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>STRENGTH: {sheet.str}</TableCell>
-                <TableCell>DEXTERITY: {sheet.dex}</TableCell>
-                <TableCell>CONSTITUTION: {sheet.con}</TableCell>
-                <TableCell>INTELLEGENCE: {sheet.int}</TableCell>
-                <TableCell>WISDOM: {sheet.wis}</TableCell>
-                <TableCell>CHARISMA: {sheet.cha}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Character Description: {sheet.charDesc}</TableCell>
-              </TableRow>
-            </TableHead>
-          </Table>
-        </TableContainer>
-      ))}
+      <Grid container>
+        {sheets.map((sheet) => (
+          <Grid>
+            <CharSheet
+              sheet={sheet}
+              key={sheet.id}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 };
